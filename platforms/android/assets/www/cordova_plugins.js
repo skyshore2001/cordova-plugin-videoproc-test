e3a481f38cb7b534d0ce7e92263cc4d4022f916b
liangjian <liangjian@oliveche.com>
2016-07-08 18:15:25 +0800

    init test project.

---
+++ platforms/android/assets/www/cordova_plugins.js
@@ -0,0 +1,19 @@
+cordova.define('cordova/plugin_list', function(require, exports, module) {
+module.exports = [
+    {
+        "file": "plugins/cordova-plugin-videoproc/www/videoproc.js",
+        "id": "cordova-plugin-videoproc.videoproc",
+        "pluginId": "cordova-plugin-videoproc",
+        "clobbers": [
+            "videoproc"
+        ]
+    }
+];
+module.exports.metadata = 
+// TOP OF METADATA
+{
+    "cordova-plugin-whitelist": "1.2.2",
+    "cordova-plugin-videoproc": "1.0.0"
+}
+// BOTTOM OF METADATA
+});
