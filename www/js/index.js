fe320838b85382803f19357fed2a1a74d104ade4
liangjian <liangjian@oliveche.com>
2016-07-08 23:16:23 +0800

    add browser platform

--- www/js/index.js
+++ www/js/index.js
@@ -49,3 +49,25 @@ var app = {
 };
 
 app.initialize();
+
+function testit()
+{
+	var opt = {
+		items: [
+			{type: 'audio', value: 'cdvfile://localhost/temporary/1.mp3'}, // 音频
+			{type: 'image', value: 'cdvfile://localhost/temporary/1.png'}, // 图片
+			{type: 'text', value: '配音: 张三丰 - 网友1', from: 15.0, to: 16.0, x: 120, y: 150}, // 文本
+			{type: 'text', value: '配音: 张无忌 - 网友2', from: 16.0, to: 17.0, x: 120, y: 150} // 文本2
+		]
+	};
+
+	videoproc.compose('cdvfile://localhost/temporary/1.mp4', opt, onSuccess, onFail);
+	
+	function onSuccess(file) {
+		alert('generate file: ' + file);
+	}
+
+	function onFail(msg) {
+		alert('fail: ' + msg);
+	}
+}
