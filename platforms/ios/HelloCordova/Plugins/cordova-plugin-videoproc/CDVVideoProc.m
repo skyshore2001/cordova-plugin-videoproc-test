5cc2a7f21580bd36f72e6b25ab084ad0caeebaf0
liangjian <liangjian@oliveche.com>
2016-07-13 15:02:37 +0800

    update platform ios

--- platforms/ios/HelloCordova/Plugins/cordova-plugin-videoproc/CDVVideoProc.m
+++ platforms/ios/HelloCordova/Plugins/cordova-plugin-videoproc/CDVVideoProc.m
@@ -7,14 +7,28 @@
 
 - (void)compose:(CDVInvokedUrlCommand*)command
 {
-    CDVPluginResult* pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK messageAsString:@"newFile"];
-
+	NSString *videoFile = [command.arguments objectAtIndex:0];
+    NSDictionary *opt = [command.arguments objectAtIndex:1];
+	
+	if (videoFile != nil && [videoFile length] == 0) {
+		NSString *errstr = @"找不到文件";
+		CDVPluginResult *result = [CDVPluginResult resultWithStatus:CDVCommandStatus_ERROR messageAsString:errstr];
+		[self.commandDelegate sendPluginResult:result callbackId:self.currentCallbackId];
+		return;
+	}
+	
+    CDVPluginResult* pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK messageAsString:videoFile];
     [self.commandDelegate sendPluginResult:pluginResult callbackId:command.callbackId];
-}
 
-+ (NSString*)cordovaVersion
-{
-    return CDV_VERSION;	
+	/*
+	[self.commandDelegate runInBackground:^{
+        NSString* payload = nil;
+        // Some blocking logic...
+        CDVPluginResult* pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK messageAsString:payload];
+        // The sendPluginResult method is thread-safe.
+        [self.commandDelegate sendPluginResult:pluginResult callbackId:command.callbackId];
+    }];
+	*/
 }
 
 @end
