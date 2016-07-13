48b5605c083b77514ed54687d9d8338f8665ce26
liangjian <liangjian@oliveche.com>
2016-07-13 14:11:03 +0800

    update android platform.

--- platforms/android/assets/www/js/index.js
+++ platforms/android/assets/www/js/index.js
@@ -52,16 +52,18 @@ app.initialize();
 
 function testit()
 {
+    var videoFile = document.getElementById("txtVideo").value;
+    var audioFile = document.getElementById("txtAudio").value;
 	var opt = {
 		items: [
-			{type: 'audio', value: 'cdvfile://localhost/temporary/1.mp3'}, // 音频
+			{type: 'audio', value: audioFile}, // 音频
 			{type: 'image', value: 'cdvfile://localhost/temporary/1.png'}, // 图片
 			{type: 'text', value: '配音: 张三丰 - 网友1', from: 15.0, to: 16.0, x: 120, y: 150}, // 文本
 			{type: 'text', value: '配音: 张无忌 - 网友2', from: 16.0, to: 17.0, x: 120, y: 150} // 文本2
 		]
 	};
 
-	videoproc.compose('cdvfile://localhost/temporary/1.mp4', opt, onSuccess, onFail);
+	videoproc.compose(videoFile, opt, onSuccess, onFail);
 	
 	function onSuccess(file) {
 		alert('generate file: ' + file);
