15084320c0cabc8b7088ba4c28bbf0394535b5e7
guanwd <@guanwdgoluk.com>
2016-07-16 23:37:57 +0800

    删除无用文件

--- platforms/ios/HelloCordova/Plugins/cordova-plugin-videoproc/CDVVideoProc.m
+++ platforms/ios/HelloCordova/Plugins/cordova-plugin-videoproc/CDVVideoProc.m
@@ -2,14 +2,27 @@
 #include <sys/sysctl.h>
 #import <Cordova/CDV.h>
 #import "CDVVideoProc.h"
-
+#ifndef GuanT_Test
+//#define GuanT_Test
+#endif
 @implementation CDVVideoProc
 
 - (void)compose:(CDVInvokedUrlCommand*)command
 {
 	NSString *videoFile = [command.arguments objectAtIndex:0];
     NSDictionary *opt = [command.arguments objectAtIndex:1];
-	
+
+#ifdef GuanT_Test
+    NSString * fileName = [[NSBundle mainBundle]pathForResource:@"config" ofType:@"json"];
+    NSString * vfileName = [[NSBundle mainBundle]pathForResource:@"2" ofType:@"MOV"];
+    VideoProc * v = [[VideoProc alloc]init];
+    NSString * jsonConfig = [NSString stringWithContentsOfFile:fileName encoding:NSUTF8StringEncoding error:nil];
+    [v compose:vfileName withConfig:jsonConfig];
+#endif
+    
+    
+    
+    
 	if (videoFile != nil && [videoFile length] == 0) {
 		NSString *errstr = @"找不到文件";
 		CDVPluginResult *result = [CDVPluginResult resultWithStatus:CDVCommandStatus_ERROR messageAsString:errstr];
