a2a50dabc70b97986efb441eba3189728252ce80
liangjian <liangjian@oliveche.com>
2016-07-20 19:15:53 -0700

    apply plugin

--- platforms/ios/HelloCordova/Plugins/cordova-plugin-videoproc/CDVVideoProc.m
+++ platforms/ios/HelloCordova/Plugins/cordova-plugin-videoproc/CDVVideoProc.m
@@ -4,7 +4,7 @@
 #import "CDVVideoProc.h"
 #import "VideoProc.h"
 #ifndef GuanT_Test
-#define GuanT_Test
+//#define GuanT_Test
 #endif
 @implementation CDVVideoProc
 
@@ -12,13 +12,22 @@
 {
 	NSString *videoFile = [command.arguments objectAtIndex:0];
     NSDictionary *opt = [command.arguments objectAtIndex:1];
+    
+    if (videoFile != nil && [videoFile length] == 0) {
+        NSString *errstr = @"找不到文件";
+        CDVPluginResult *result = [CDVPluginResult resultWithStatus:CDVCommandStatus_ERROR messageAsString:errstr];
+        [self.commandDelegate sendPluginResult:result callbackId:command.callbackId];
+        return;
+    }
+    
 #ifdef GuanT_Test
     videoFile = [[NSBundle mainBundle]pathForResource:@"2" ofType:@"MOV"];
     
     NSString * optdes = [[NSBundle mainBundle]pathForResource:@"config" ofType:@"json"];
     NSData * data = [NSData dataWithContentsOfFile:optdes];
     opt = [NSJSONSerialization JSONObjectWithData:data options:NSJSONReadingAllowFragments error:nil];
-    
+#endif
+
     __weak CDVVideoProc * wself= self;
     [self.commandDelegate runInBackground:^{
         VideoProc * v = [[VideoProc alloc]init];
@@ -31,21 +40,6 @@
             [wself.commandDelegate sendPluginResult:result callbackId:command.callbackId];
         }];
     }];
-#endif
-    
-    
-    
-    
-	if (videoFile != nil && [videoFile length] == 0) {
-		NSString *errstr = @"找不到文件";
-		CDVPluginResult *result = [CDVPluginResult resultWithStatus:CDVCommandStatus_ERROR messageAsString:errstr];
-		[self.commandDelegate sendPluginResult:result callbackId:command.callbackId];
-		return;
-	}
-
-    
-
-
 }
 
 @end
